7ad5a59533a82e24a1d5ed6fae28f146056ead57
Alka Wilson <alkarhine@gmail.com>
2024-08-03 21:09:14 -0400

    Resolved merge conflicts

---
+++ base/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+# models work with database 
+class Topic(models.Model): # here topic can have multiple rooms - one to many
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Room(models.Model):
+    host =  models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+     
+    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True) 
+
+    name = models.CharField(max_length=200)
+    descriptions = models.TextField(null=True,blank = True) # null is allowed
+    #participants 
+    updated = models.DateTimeField(auto_now=True) # takes a snapshot every time we save something
+    created = models.DateTimeField(auto_now_add=True) # take a snapshot only once it gets created 
+
+    def __str__(self): 
+        return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE) # one to many relationship - a user can have many messages but all those messages belong to one user
+
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True) # takes a snapshot every time we save something
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
---
+++ base/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Room
+
+# Create your views here.
+
+# rooms =  [
+#     {'id':1,'name': 'Lets learn python!'},
+#     {'id':2,'name': 'Design with me'},
+#     {'id':3,'name': 'Frontend developers'},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    return render(request,'base/home.html',{'rooms':rooms})
+
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    return render(request,'base/room.html',  {'room': room})
