89e38541e0839de84b801cf88b79293cd43f2a54
Alka Wilson <alkarhine@gmail.com>
2024-08-03 21:10:49 -0400

    added the files successfully

--- base/views.py
+++ base/views.py
@@ -8,7 +8,6 @@ from .models import Room
 #     {'id':2,'name': 'Design with me'},
 #     {'id':3,'name': 'Frontend developers'},
 # ]
-
 def home(request):
     rooms = Room.objects.all()
     return render(request,'base/home.html',{'rooms':rooms})
